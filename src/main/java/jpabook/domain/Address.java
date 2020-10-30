cdfcc5ae713f8534a80515d333ce547128ba5b5a
kwkim <ruddn0208@gmail.com>
2020-10-30 19:27:50 +0900

    shopping mall enetity 설계중

--- src/main/java/jpabook/domain/Address.java
+++ src/main/java/jpabook/domain/Address.java
@@ -7,7 +7,14 @@ import javax.persistence.Embeddable;
 @Embeddable
 @Getter
 public class Address {
+
     private String city;
     private String  street;
     private String zipcoode;
+    protected  Address(){}
+    public Address(String city,String street,String zipcoode){
+        this.city=city;
+        this.street=street;
+        this.zipcoode=zipcoode;
+    }
 }
--- src/main/java/jpabook/domain/item/Item.java
+++ src/main/java/jpabook/domain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.domain.item;
 
+import jpabook.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,4 +20,7 @@ public  abstract class Item {
     private Long id;
     private String name;
     private  int stockQuantity;
+    @ManyToMany(mappedBy = "items")
+    private List<Category>categories=new ArrayList<>();
+
 }
